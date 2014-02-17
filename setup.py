4181cd75b37bd951225812a072f47b997442a72c
Alex Kir <alex@wall-dev.org>
2014-02-17 14:26:27 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,13 @@ from setuptools import setup
 setup(
     name='Flask-MSTrans',
     version='0.1',
-    url='',
+    url='https://github.com/osleg/Flask-MsTranslate/archive/master.zip',
     license='BSD',
     author='Alex Kir',
     author_email='alex@wall-dev.org',
     description='Translate text using Bing',
     long_description=__doc__,
     py_modules=['flask_mstrans'],
-    # if you would be using a package instead use packages instead
-    # of py_modules:
-    # packages=['flask_sqlite3'],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
